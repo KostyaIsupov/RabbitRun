0c309f78b931096534b32837d1b6685d3c1a973f
KostyaIsupov <kostyaisupov1113@gmail.com>
2023-11-11 17:13:03 +0300

    health heals

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameUIController : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private KeyCode pauseKey = KeyCode.Escape;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            OpenPausePanel();
+        }
+    }
+
+    private void OpenPausePanel()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ClosePausePanel()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void ExitGame()
+    {
+        
+    }    
+}
